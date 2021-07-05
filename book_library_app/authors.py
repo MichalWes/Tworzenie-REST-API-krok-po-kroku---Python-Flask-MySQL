14a9a818cdbe9971b35605a8b092190763d33f4e
MichalWes <massive7m@gmail.com>
2021-07-05 17:25:29 +0200

    Update authors

--- book_library_app/authors.py
+++ book_library_app/authors.py
@@ -2,6 +2,7 @@ from flask import jsonify, request
 from webargs.flaskparser import use_args
 from book_library_app import app, db
 from book_library_app.models import Author, AuthorSchema, author_schema
+from book_library_app.utils import validate_json_content_type
 
 
 @app.route('/api/v1/authors', methods=['GET'])
@@ -26,7 +27,8 @@ def get_author(author_id: int):
 
 
 @app.route('/api/v1/authors', methods=['POST'])
-@use_args(author_schema)
+@validate_json_content_type
+@use_args(author_schema, error_status_code=400)
 def create_author(args: dict):
     author = Author(**args)
     db.session.add(author)
@@ -39,7 +41,11 @@ def create_author(args: dict):
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['PUT'])
-def update_author(author_id: int):
+@validate_json_content_type
+@use_args(author_schema, error_status_code=400)
+def update_author(args: dict, author_id: int):
+    author = Author.query.get_or_404(
+        author_id, description=f'Author with id {author_id} not found')
     return jsonify({
         'success': True,
         'data': f'Author with id {author_id} has been updated'
